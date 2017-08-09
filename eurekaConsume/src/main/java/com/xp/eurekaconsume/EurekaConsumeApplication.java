406e0f943c742cec6e753c91acfce0ffed278e0d
coderXP <370256646@qq.com>
2017-08-09 15:50:31 +0800

    Eureka服务消费者

---
+++ eurekaConsume/src/main/java/com/xp/eurekaconsume/EurekaConsumeApplication.java
@@ -0,0 +1,12 @@
+package com.xp.eurekaconsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConsumeApplication.class, args);
+	}
+}
