6d01a032e98a918112ecba05678521fadab655f9
coderXP <370256646@qq.com>
2017-08-11 17:43:23 +0800

    Feign服务消费者

--- feignConsume/src/main/java/com/xp/feignconsume/controller/ConsumeController.java
+++ feignConsume/src/main/java/com/xp/feignconsume/controller/ConsumeController.java
@@ -1,5 +1,6 @@
 package com.xp.feignconsume.controller;
 
+import com.xp.feignconsume.model.User;
 import com.xp.feignconsume.service.IHelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,5 +15,21 @@ public class ConsumeController {
     @RequestMapping(value = "feign-consume",method = RequestMethod.GET)
     public String HelloConsumer(){
         return helloService.hello();
+    }
+
+
+    @RequestMapping(value = "feign-consume-param",method = RequestMethod.GET)
+    public String HelloWithParamConsumer(){
+        return helloService.hello("xp");
+    }
+
+    @RequestMapping(value = "feign-consume-header-param",method = RequestMethod.GET)
+    public String HelloWithHeaderParamConsumer(){
+        return helloService.hello("xp",24);
+    }
+
+    @RequestMapping(value = "feign-consume-body-param",method = RequestMethod.GET)
+    public String HelloWithBodyParamConsumer(){
+        return helloService.hello(new User("xp",24));
     }
 }
--- feignConsume/src/main/java/com/xp/feignconsume/service/IHelloService.java
+++ feignConsume/src/main/java/com/xp/feignconsume/service/IHelloService.java
@@ -1,11 +1,22 @@
 package com.xp.feignconsume.service;
 
+import com.xp.feignconsume.model.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient("hello-service")
 public interface IHelloService {
 
     @RequestMapping("/hello")
     String hello();
+
+    //feign绑定参数示例,不要求方法名和接口提供的方法名称一致
+    @RequestMapping(value="/hello-param",method = RequestMethod.GET)
+    String hello(@RequestParam("name") String name);
+
+    @RequestMapping(value = "/hello-headerParam",method = RequestMethod.GET)
+    String hello(@RequestHeader("name") String name,@RequestHeader("age") Integer age);
+
+    @RequestMapping(value = "/hello-bodyParam",method = RequestMethod.POST)
+    String hello(@RequestBody User user);
 }
