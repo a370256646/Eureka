886fbf264ac19d162334720be5b5b9e799bb2be1
coderXP <370256646@qq.com>
2017-08-11 17:43:12 +0800

    Eureka服务提供者

--- eureka_client/src/main/java/com/xp/eureka_client/controller/HelloController.java
+++ eureka_client/src/main/java/com/xp/eureka_client/controller/HelloController.java
@@ -1,11 +1,11 @@
 package com.xp.eureka_client.controller;
 
 
+import com.xp.eureka_client.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -17,4 +17,23 @@ public class HelloController {
         ServiceInstance instance=client.getLocalServiceInstance();
         return "hello world!host is :"+instance.getHost()+",serviceId is :"+instance.getServiceId();
     }
+
+
+    //方法扩展,学习feign参数绑定
+    @RequestMapping(value = "/hello-param",method = RequestMethod.GET)
+    public String helloWithParam(@RequestParam String name){
+        return "this is param method,hello "+name;
+    }
+
+    @RequestMapping(value = "/hello-headerParam",method = RequestMethod.GET)
+    public String helloWithHeadParam(@RequestHeader String name){
+        return "this is header param method,hello "+name;
+    }
+
+    @RequestMapping(value = "/hello-bodyParam",method = RequestMethod.POST)
+    public String helloWithBody(@RequestBody User user){
+        return "hello,"+user.getName()+","+user.getAge();
+    }
+
+
 }
